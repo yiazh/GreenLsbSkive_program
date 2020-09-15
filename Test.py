38f7f6ea1ca724f1c5b04c372997707871c4c01a
yiazh <65192691+yiazh@users.noreply.github.com>
2020-09-15 12:34:20 +0200

    A test

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ Created on: ********
 
 Author: Yi Zheng, Department of Electrical Engineering, DTU
 
-Some confused syntax is tested in this file.1
+Some confused syntax is tested in this file.
 '''
 
 # Understanding multiple inheritance
